11f664e0439ab867dd3533cf90c288816a220727
Jason Gibb <jason.gibb@bemaservices.com>
2017-12-21 17:02:51 -0600

    Adding V5 Conversion Scripts
    These scripts are our most up-to-date SQL queries for V5 conversions as of 12/21/2017.

---
+++ Slingshot.V5/SQL Queries/person-attributevalue.sql
@@ -0,0 +1,89 @@
+SELECT 
+	-- PersonId --
+	[NameCounter] AS [PersonId],
+	-- AttributeKey --
+	CASE
+	WHEN [Profile] = 'SCHOOL' THEN 'V5SCHOOL'
+	ELSE [Profile]
+	END as [AttributeKey],
+	-- AttributeValue --
+	--REPLACE(ad.Adr1, ',', ' ')
+	CASE
+		WHEN ([Comment] IS NULL OR [Comment] = '') AND ([Start] IS NOT NULL) THEN ISNULL(REPLACE(REPLACE(dbo.KeepSafeCharacters([Comment], 1),CHAR(13),''),CHAR(10),''),'TRUE')
+		ELSE ISNULL(REPLACE(REPLACE([Comment], CHAR(13), ''), CHAR(10), ''),'TRUE')
+	END AS [AttributeValue]
+
+FROM [Shelby].[NAProfiles]
+WHERE [Profile] IN (
+'2012',
+'2013',
+'2014',
+--'ALLERG', -- We have imported this into an Allergy Note
+--'AUTODONE',
+--'AUTONCNS',
+'AUTONOND',
+--'BENAPP',
+--'BENDEN',
+'CAREAUTO',
+'CARECLN',
+'CAREERND',
+'CAREHOSP',
+'CAREMEAL',
+/*  -- We have imported the folloewing into Rock's Graduation Year
+'GRADE1',
+'GRADE10',
+'GRADE11',
+'GRADE12',
+'GRADE2',
+'GRADE3',
+'GRADE4',
+'GRADE5',
+'GRADE6',
+'GRADE7',
+'GRADE8',
+'GRADE9',
+'GRADEK',
+*/
+
+'GRADES',
+'LASTCN',
+'MAIL',
+'MENATWRK',
+--'NOTE1', -- We have imported this into a General Note
+--'QDATA', -- We have imported this into a General Note (Set as Private)
+'SCHOOL',
+'SLA3K',
+'SLACH',
+'SLAEC',
+'SLAEL',
+'SLD3K',
+'SLDCH',
+'SLDEC',
+'SLDEL',
+'SLL12',
+'SLL35',
+'SLL3YO',
+'SLL4YO',
+'SLL5K',
+'SLLCH',
+'SLLCOF',
+'SLLEC',
+'SLLM3K',
+'SLLMEC',
+'SLLMEL',
+'SLLSUB',
+'SLW12',
+'SLW1YO',
+'SLW2YO',
+'SLW35',
+'SLW3YO',
+'SLW4YO',
+'SLW5YO',
+'SLWCH',
+'SLWCOF',
+'SLWINF',
+'SLWK',
+'SLWSUB',
+--'SPECIA', -- We have imported this into a Medical Note
+'STAFF'
+)
---
+++ Slingshot.V5/SQL Queries/person-note.sql
@@ -0,0 +1,103 @@
+SELECT 
+	-- PersonId --
+	n.[NameCounter] AS [PersonId],
+	-- Id -- 
+		CASE
+		WHEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p1.Counter
+		WHEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p2.Counter
+		WHEN REPLACE(REPLACE(p3.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p3.Counter
+		WHEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p4.Counter
+		WHEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p5.Counter
+		WHEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p6.Counter
+		WHEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p7.Counter
+		WHEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p8.Counter
+		WHEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN p9.Counter
+		END AS [Id],
+	-- NoteType --
+	CASE
+	--SELECT REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Allergy Note'
+		WHEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'General Note'
+		WHEN REPLACE(REPLACE(p3.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'General Note'
+		WHEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Medical Note'
+
+		WHEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'CMHost Note'
+		WHEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Benevolence Note'
+		WHEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Benevolence Note'
+		WHEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Auto Note'
+		WHEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'Auto Note'
+		ELSE ''
+	END AS [NoteType],
+	-- Caption --
+	'' AS [Caption],
+	-- IsAlert --
+	CASE
+		WHEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'TRUE'
+		WHEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p3.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'TRUE'
+
+		WHEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		ELSE ''
+	END AS [IsAlert],
+	-- IsPrivate --
+	CASE
+		WHEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p3.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'TRUE'
+		WHEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+
+		WHEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		WHEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'FALSE'
+		ELSE ''
+	END as [IsPrivateNote],
+	-- Text --
+	CASE
+		WHEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p1.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p2.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p3.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN 'See Shelby'
+		WHEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p4.[Comment], CHAR(13), ''), CHAR(10), '')
+		
+		WHEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p5.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p6.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p7.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p8.[Comment], CHAR(13), ''), CHAR(10), '')
+		WHEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '') IS NOT NULL THEN REPLACE(REPLACE(p9.[Comment], CHAR(13), ''), CHAR(10), '')
+		ELSE ''
+	END as [Text],
+	-- DateTime --
+	'' AS [DateTime],
+	-- CreateByPersonId --
+	'' AS [CreatedByPersonId]
+
+FROM [Shelby].[NANames] n
+LEFT OUTER JOIN [Shelby].[NAProfiles] p1 ON n.[NameCounter] = p1.[NameCounter] AND p1.[Profile] = 'ALLERG'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p2 ON n.[NameCounter] = p2.[NameCounter] AND p2.[Profile] = 'NOTE1'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p3 ON n.[NameCounter] = p3.[NameCounter] AND p3.[Profile] = 'QDATA'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p4 ON n.[NameCounter] = p4.[NameCounter] AND p4.[Profile] = 'SPECIA'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p5 ON n.[NameCounter] = p5.[NameCounter] AND p5.[Profile] = 'CM24H'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p6 ON n.[NameCounter] = p6.[NameCounter] AND p6.[Profile] = 'BENDEN'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p7 ON n.[NameCounter] = p7.[NameCounter] AND p7.[Profile] = 'BENAPP'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p8 ON n.[NameCounter] = p8.[NameCounter] AND p8.[Profile] = 'AUTONCNS'
+LEFT OUTER JOIN [Shelby].[NAProfiles] p9 ON n.[NameCounter] = p9.[NameCounter] AND p9.[Profile] = 'AUTODONE'
+
+WHERE 
+-- Limiting to ONLY the 9 Note Types
+p1.[Comment] IS NOT NULL OR
+p2.[Comment] IS NOT NULL OR
+p3.[Comment] IS NOT NULL OR
+p4.[Comment] IS NOT NULL OR
+p5.[Comment] IS NOT NULL OR
+p6.[Comment] IS NOT NULL OR
+p7.[Comment] IS NOT NULL OR
+p8.[Comment] IS NOT NULL OR
+p9.[Comment] IS NOT NULL
+
+ORDER BY [PersonId]
