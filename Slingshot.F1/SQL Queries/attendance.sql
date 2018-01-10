b255a7d63b14c81fe472f188f376ec2d32e2bb4f
Jason Gibb <jason.gibb@bemaservices.com>
2018-01-09 22:13:15 -0700

    Adding F1 SQL Queries
    These queries are to be ran against the MDF export and are useful to pull attendance data from F1 into Rock.

---
+++ Slingshot.F1/SQL Queries/attendance.sql
@@ -0,0 +1,13 @@
+SELECT
+ROW_NUMBER() OVER(ORDER BY Start_Date_Time) AS [AttendanceId]
+,Individual_ID AS [PersonId]
+,RLC_ID AS [GroupId]
+,'' AS [LocationId]
+,'' AS [ScheduleId]
+,'' AS [DeviceId]
+,Start_Date_Time AS [StartDateTime]
+,'' AS [EndDateTime]
+,'' AS [Note]
+,'' AS [CampusId]
+
+FROM Attendance
