31e7b6c7f4d18028b0278323c9fcc0b6cf1865b6
mkinyon <mavhuntrx@gmail.com>
2018-01-23 15:15:27 -0700

    Elexio - Fixed issue with attendance campus not exporting correctly

--- Slingshot.Elexio/Slingshot.Elexio/Utilities/Translators/ElexioAttendance.cs
+++ Slingshot.Elexio/Slingshot.Elexio/Utilities/Translators/ElexioAttendance.cs
@@ -23,6 +23,12 @@ namespace Slingshot.Elexio.Utilities.Translators
                 attendance.GroupId = groupId;
             }
 
+            var campusId = row.Field<int?>( "CampusId" );
+            if ( campusId.HasValue )
+            {
+                attendance.CampusId = campusId;
+            }
+
             var locationId = row.Field<int?>( "LocationId" );
             if ( locationId.HasValue )
             {
