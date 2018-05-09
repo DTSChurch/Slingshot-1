cd5e8dfd3c42992b0e3eadb4f9645de5fd539834
mkinyon <mavhuntrx@gmail.com>
2018-05-09 15:41:02 -0700

    Re-wrote the The City Slingshot tool to use the same design patterns as the other Slingshot tools.  Still very much a work in progress.
    So far it can extract some portions of people data VERY slowly.

--- Slingshot.TheCity/Slingshot.TheCity/Utilities/TheCityApi.cs
+++ Slingshot.TheCity/Slingshot.TheCity/Utilities/TheCityApi.cs
@@ -1,100 +1,276 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Security.Cryptography;
 using System.Text;
+using System.Web;
+using System.Xml;
 using System.Xml.Linq;
-using RestSharp;
 using Newtonsoft.Json;
-using System.Collections.Generic;
+using RestSharp;
+using RestSharp.Authenticators;
+using RestSharp.Authenticators.OAuth;
+//using RestSharp.Extensions.MonoHttp;
+using Slingshot.Core;
+using Slingshot.Core.Model;
+using Slingshot.Core.Utilities;
+using Slingshot.TheCity.Utilities.Translators;
 
 namespace Slingshot.TheCity.Utilities
 {
+    /// <summary>
+    /// API F1 Status
+    /// </summary>
     public static class TheCityApi
     {
-
-        private static string secretKey { get; set; } = "6dbb51307527dd14cced2e529e839b23db761d86";
-        private static string userToken { get; set; } = "07c4b44282a4a217";
-        private static int ApiCounter { get; set; } = 0;
-        private static string ApiUrl { get; set; } = "https://api.onthecity.org";
-
         private static RestClient _client;
+        private static RestRequest _request;
+        private static int loopThreshold = 100;
 
         /// <summary>
-        /// Exports the users
+        ///  Set F1Api.DumpResponseToXmlFile to true to save all API Responses 
+        ///   to XML files and include them in the slingshot package
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if the response should get dumped to XML; otherwise, <c>false</c>.
+        /// </value>
+        public static bool DumpResponseToXmlFile { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last run date.
+        /// </summary>
+        /// <value>
+        /// The last run date.
+        /// </value>
+        public static DateTime LastRunDate { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Gets or sets the api counter.
+        /// </summary>
+        /// <value>
+        /// The api counter.
+        /// </value>
+        public static int ApiCounter { get; set; } = 0;
+
+        /// <summary>
+        /// Gets or sets the error message.
+        /// </summary>
+        /// <value>
+        /// The error message.
+        /// </value>
+        public static string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// Gets the API URL.
+        /// </summary>
+        /// <value>
+        /// The API URL.
+        /// </value>
+        public static string ApiUrl
+        {
+            get
+            {
+                return "https://api.onthecity.org";
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the API consumer key.
+        /// </summary>
+        /// <value>
+        /// The API consumer key.
+        /// </value>
+        public static string ApiKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the API consumer secret.
+        /// </summary>
+        /// <value>
+        /// The API consumer secret.
+        /// </value>
+        public static string ApiToken { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is connected.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance is connected; otherwise, <c>false</c>.
+        /// </value>
+        public static bool IsConnected { get; private set; } = false;
+
+        #region API Call Paths 
+
+        private const string API_USERS = "/users";
+
+        #endregion
+
+        /// <summary>
+        /// Initializes the export.
+        /// </summary>
+        public static void InitializeExport()
+        {
+            ImportPackage.InitalizePackageFolder();
+        }
+
+        /// <summary>
+        /// Connects the specified host name.
+        /// </summary>
+        /// <param name="hostName">Name of the host.</param>
+        /// <param name="apiUsername">The API username.</param>
+        /// <param name="apiPassword">The API password.</param>
+        public static void Connect( string apiKey, string apiToken )
+        {
+            ApiKey = apiKey;
+            ApiToken = apiToken;
+
+            int timestamp = GetTimeStamp();
+            string HMAC = GenerateHMAC( "GET", ApiUrl + API_USERS, "/count", timestamp );
+
+            _client = new RestClient( ApiUrl );
+            _request = new RestRequest( API_USERS + "/count", Method.GET );
+            _request.AddHeader( "X-City-Sig", HMAC );
+            _request.AddHeader( "X-City-User-Token", ApiToken );
+            _request.AddHeader( "X-City-Time", timestamp.ToString() );
+            _request.AddHeader( "Accept", "application/vnd.thecity.admin.v1+json" );
+
+            // getting the api status sets the IsConnect flag
+            UpdateApiStatus();
+        }
+
+        /// <summary>
+        /// Updates the API status.
+        /// </summary>
+        public static void UpdateApiStatus()
+        {
+            // execute the request to get a oauth token and secret
+            var response = _client.Execute( _request );
+
+            if ( response.StatusCode == System.Net.HttpStatusCode.OK )
+            {
+                IsConnected = true;
+            }
+            else
+            {
+                ErrorMessage = response.Content;
+            }
+        }
+
+        /// <summary>
+        /// Exports the individuals.
         /// </summary>
         /// <param name="modifiedSince">The modified since.</param>
         /// <param name="peoplePerPage">The people per page.</param>
-        public static void ExportUsers( DateTime modifiedSince )
+        public static void ExportIndividuals( DateTime modifiedSince )
         {
-            const string uri = "users";
-            int totalPages = 0;
             int currentPage = 1;
-            bool done = false;
+            int loopCounter = 0;
+            bool moreIndividualsExist = true;
 
-            while ( !done || currentPage < 100 )
+            try
             {
-                // Parsing Parameters
-                string modifiedDate = modifiedSince.ToString( "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture );
-                string queryParameters = string.Format( "?filter=created_between_{0}_{1}&include_participation=true&page={2}", modifiedDate, DateTime.Now.ToString( "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture ), currentPage);
-
-                // Creating timestamp
-                int timestamp = ( int ) ( DateTime.UtcNow.Subtract( new DateTime( 1970, 1, 1 ) ) ).TotalSeconds;
-
-                // Getting HMAC
-                string HMAC = GenerateHMAC( "GET", ApiUrl + "/" + uri, queryParameters, timestamp );
-
-                // Creating client and request
-                _client = new RestClient( ApiUrl );
-                var request = new RestRequest( uri + queryParameters, Method.GET );
-                request.AddHeader( "X-City-Sig", HMAC );
-                request.AddHeader( "X-City-User-Token", userToken );
-                request.AddHeader( "X-City-Time", timestamp.ToString() );
-                request.AddHeader( "Accept", "application/vnd.thecity.admin.v1+json" );
-
-                // Making request
-                var restResponse = _client.Execute( request );
-
-                // Making sure request was successful
-                if (restResponse.StatusCode == System.Net.HttpStatusCode.OK )
+                while ( moreIndividualsExist )
                 {
-                    // Getting Data from request
-                    Models.UsersModel users = JsonConvert.DeserializeObject<Models.UsersModel>( restResponse.Content );
-                    var rateLimitRemaining = restResponse.Headers.Where( x => x.Name == "X-City-Ratelimit-Remaining-By-Account" ).Single().Value;
+                    string queryParameters = string.Format( "?filter=updated_between_{0}_{1}&page={2}",
+                        modifiedSince.ToString( "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture ) , DateTime.Now.ToString( "yyyyMMdd", System.Globalization.CultureInfo.InvariantCulture ), currentPage );
 
-                    // Checking if this is the initial iteration
-                    if (totalPages == 0 )
+                    int timestamp = GetTimeStamp();
+                    string HMAC = GenerateHMAC( "GET", ApiUrl + API_USERS, queryParameters, timestamp );
+
+                    _client = new RestClient( ApiUrl );
+                    _request = new RestRequest( API_USERS + queryParameters, Method.GET );
+                    _request.AddHeader( "X-City-Sig", HMAC );
+                    _request.AddHeader( "X-City-User-Token", ApiToken );
+                    _request.AddHeader( "X-City-Time", timestamp.ToString() );
+                    _request.AddHeader( "Accept", "application/vnd.thecity.admin.v1+json" );
+
+                    var response = _client.Execute( _request );
+                    ApiCounter++;
+
+                    XDocument xdoc = XDocument.Load( JsonReaderWriterFactory.CreateJsonReader(
+                        Encoding.ASCII.GetBytes( response.Content ), new XmlDictionaryReaderQuotas() ) );
+
+                    if ( TheCityApi.DumpResponseToXmlFile )
                     {
-                        totalPages = users.TotalPages;
-                        currentPage++;
+                        xdoc.Save( Path.Combine( ImportPackage.PackageDirectory, $"API_INDIVIDUALS_ResponseLog_{loopCounter}.xml" ) );
+                    }
+
+                    var records = xdoc.Element( "root" ).Element( "users" );
+
+                    if ( records != null )
+                    {
+                        var returnCount = xdoc.Element( "root" ).Element( "per_page" )?.Value.AsIntegerOrNull();
+                        var additionalPages = xdoc.Element( "root" ).Element( "total_pages" ).Value.AsInteger();
+
+                        if ( returnCount.HasValue )
+                        {
+                            foreach ( var personNode in records.Elements() )
+                            {
+                                if ( personNode.Element( "id" ) != null && personNode.Element( "id" ).Value.AsIntegerOrNull() > 0 && 
+                                     personNode.Element( "type" ).Value != "OfflineUser" && personNode.Element( "last" ).Value.Length > 1 )
+                                {
+                                    // get family
+                                    int personId = personNode.Element( "id" ).Value.AsInteger();
+                                    int fmtimestamp = GetTimeStamp();
+                                    string fmHMAC = GenerateHMAC( "GET", ApiUrl + API_USERS + "/" + personId.ToString() + "/family", "", fmtimestamp );
+
+                                    _client = new RestClient( ApiUrl );
+                                    _request = new RestRequest( API_USERS + "/" + personId.ToString() + "/family", Method.GET );
+                                    _request.AddHeader( "X-City-Sig", fmHMAC );
+                                    _request.AddHeader( "X-City-User-Token", ApiToken );
+                                    _request.AddHeader( "X-City-Time", fmtimestamp.ToString() );
+                                    _request.AddHeader( "Accept", "application/vnd.thecity.admin.v1+json" );
+
+                                    var fmresponse = _client.Execute( _request );
+                                    ApiCounter++;
+
+                                    XDocument fmxdoc = XDocument.Load( JsonReaderWriterFactory.CreateJsonReader(
+                                        Encoding.ASCII.GetBytes( fmresponse.Content ), new XmlDictionaryReaderQuotas() ) );
+
+
+                                    var importPerson = TheCityPerson.Translate( personNode, fmxdoc.Element( "root" ) );
+
+                                    if ( importPerson != null )
+                                    {
+                                        ImportPackage.WriteToPackage( importPerson );
+                                    }
+                                }
+                            }
+
+                            if ( additionalPages <= 0 && returnCount <= 0 )
+                            {
+                                moreIndividualsExist = false;
+                            }
+                            else
+                            {
+                                currentPage++;
+                            }
+                        }
                     }
                     else
                     {
-                        currentPage++;
+                        moreIndividualsExist = false;
                     }
 
-                    // Looping through people
-                    foreach(var user in users.Users )
+                    // developer safety blanket (prevents eating all the api calls for the day) 
+                    if ( loopCounter > loopThreshold )
                     {
-                        // Checking if user is an offline "system" user
-                        if( (user.Type != "OfflineUser") && (user.Last.Length != 1) )
-                        {
-                            // Here is where the transforms need to be added
-
-                            Console.WriteLine( string.Format( "{0},{1} ({2}) - {3} - {4}", user.Last, user.First, user.Type, user.Email, user.CreatedAt ) );
-                        }
+                        break;
                     }
-
-                    Console.WriteLine( "##############" + rateLimitRemaining + "##############" );
-                }
-                else
-                {
-                    throw new System.Exception( "Request to The City was not successful. HTTP Status code: " + restResponse.StatusCode.ToString() );
+                    loopCounter++;
                 }
 
-                // Incrementing API Counter
-                ApiCounter++;
             }
+            catch ( Exception ex )
+            {
+                ErrorMessage = ex.Message;
+            }
+        }
+
+        private static int GetTimeStamp()
+        {
+            return ( int ) ( DateTime.UtcNow.Subtract( new DateTime( 1970, 1, 1 ) ) ).TotalSeconds;
         }
 
         /// <summary>
@@ -115,24 +291,8 @@ namespace Slingshot.TheCity.Utilities
             // Concatenating time + verb + host + path + query_params + body
             var stringToSign = string.Format( "{0}{1}{2}{3}", timestamp, httpVerb, uri, queryParameters );
 
-            // Constructing HMAC signature
-            /*
-            var encoding = GetIso88591Encoding();
-            byte[] secretKeyBytes = Encoding.Default.GetBytes( secretKey );
-            HMACSHA256 HMACSha = new HMACSHA256( secretKeyBytes );
-            byte[] inputByteArray = encoding.GetBytes( stringToSign );
-            MemoryStream stream = new MemoryStream( inputByteArray );
-            byte[] hashValue = HMACSha.ComputeHash( stream );
-            string unencodedHMAC = encoding.GetString( hashValue );
-
-            byte[] unencodedHMACBytes = encoding.GetBytes( unencodedHMAC );
-            string unescapedHMAC = Convert.ToBase64String( unencodedHMACBytes ).Trim();
-
-            string HMACSignature = Uri.EscapeDataString( unescapedHMAC );
-            */
-
             var encoding = Encoding.UTF8;
-            var HMACSha256 = new HMACSHA256( encoding.GetBytes( secretKey ) );
+            var HMACSha256 = new HMACSHA256( encoding.GetBytes( ApiKey ) );
             var stream = new MemoryStream( encoding.GetBytes( stringToSign ) );
             var unencodedHMAC = HMACSha256.ComputeHash( stream );
 
@@ -143,18 +303,6 @@ namespace Slingshot.TheCity.Utilities
             var HMACSignature = Uri.EscapeDataString( unescapedHMAC );
 
             return HMACSignature;
-        }
-
-        private static System.Text.Encoding GetIso88591Encoding()
-        {
-            var encodingInfo = System.Text.Encoding.GetEncodings().FirstOrDefault( e => e.Name.ToUpperInvariant() == "ISO-8859-1" );
-
-            if ( encodingInfo == null )
-            {
-                throw new Exception( "Could not retrieve ISO-8859-1 text encoding" );
-            }
-
-            return encodingInfo.GetEncoding();
         }
     }
 }
---
+++ Slingshot.TheCity/Slingshot.TheCity/Utilities/Translators/TheCityPerson.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Slingshot.Core;
+using Slingshot.Core.Model;
+
+namespace Slingshot.TheCity.Utilities.Translators
+{
+    public static class TheCityPerson
+    {
+        public static Person Translate( XElement inputPerson, XElement inputFamily )
+        {
+            var person = new Person();
+            var notes = new List<string>();
+
+            person.Id = inputPerson.Element( "id" ).Value.AsInteger();
+
+            // names
+            person.FirstName = inputPerson.Element( "first" ).Value;
+            person.NickName = inputPerson.Element( "nickname" )?.Value;
+            person.MiddleName = inputPerson.Element( "middle" )?.Value;
+            person.LastName = inputPerson.Element( "last" )?.Value;
+            person.Salutation = inputPerson.Element( "title" )?.Value;
+
+            // gender
+            var gender = inputPerson.Element( "gender" )?.Value;
+            if ( gender.IsNotNullOrWhitespace() )
+            {
+                switch ( gender )
+                {
+                    case "Male":
+                        person.Gender = Gender.Male;
+                        break;
+                    case "Female":
+                        person.Gender = Gender.Female;
+                        break;
+                    default:
+                        person.Gender = Gender.Unknown;
+                        break;
+                }
+            }
+
+            // email
+            person.Email = inputPerson.Element( "email" )?.Value;
+
+            // primary phone
+            var primaryPhone = inputPerson.Element( "primary_phone" )?.Value;
+            if ( primaryPhone.IsNotNullOrWhitespace() )
+            {
+                person.PhoneNumbers.Add( new PersonPhone
+                {
+                    PersonId = person.Id,
+                    PhoneType = inputPerson.Element( "primary_phone_type" )?.Value,
+                    PhoneNumber = primaryPhone
+                } );
+            }
+
+            // secondary phone
+            var secondaryPhone = inputPerson.Element( "secondary_phone_type" )?.Value;
+            if ( secondaryPhone.IsNotNullOrWhitespace() )
+            {
+                person.PhoneNumbers.Add( new PersonPhone
+                {
+                    PersonId = person.Id,
+                    PhoneType = inputPerson.Element( "secondary_phone_type" )?.Value,
+                    PhoneNumber = secondaryPhone
+                } );
+            }
+
+
+            // addresses
+
+            // family 
+            var familyId = inputFamily.Element( "id" )?.Value.AsIntegerOrNull();
+
+
+            if ( familyId > 0 )
+            {
+                person.FamilyId = familyId;
+                person.FamilyName = person.LastName + " Family";
+
+                var familyMembers = inputFamily.Element( "family_members" );
+                foreach ( var familyMember in familyMembers.Elements() )
+                {
+                    var personId = familyMember.Element( "user_id" ).Value.AsInteger();
+                    if ( person.Id == personId )
+                    {
+                        var familyRole = familyMember.Element( "family_role" ).Value;
+                        switch ( familyRole )
+                        {
+                            case "Spouse":
+                                person.FamilyRole = FamilyRole.Adult;
+                                break;
+                            case "Child":
+                                person.FamilyRole = FamilyRole.Child;
+                                break;
+                            default:
+                                person.FamilyRole = FamilyRole.Child;
+                                person.Note += "FamilyRole: " + familyRole;
+                                break;
+                        }
+                    }
+                } 
+            }
+
+            // campus
+            var campusId = inputPerson.Element( "primary_campus_id" )?.Value.AsIntegerOrNull();
+            if ( campusId.HasValue )
+            {
+                person.Campus = new Campus
+                {
+                    CampusId = campusId.Value,
+                    CampusName = inputPerson.Element( "primary_campus_name" ).Value
+                };
+            }
+
+            // dates
+            person.Birthdate = inputPerson.Element( "birthdate" )?.Value.AsDateTime();
+            person.CreatedDateTime = inputPerson.Element( "created_at" )?.Value.AsDateTime();
+            person.ModifiedDateTime = inputPerson.Element( "updated_at" )?.Value.AsDateTime();
+
+            // debug
+            person.Note = inputPerson.Element( "type" )?.Value;
+
+            return person;
+        }
+    }
+}
