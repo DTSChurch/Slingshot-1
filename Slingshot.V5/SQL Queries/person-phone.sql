eaab62a404bbd1c8485b66a594b1c24d7e2b70c3
Joshua Maiorino <33353876+JoshuaMaiorino@users.noreply.github.com>
2019-01-02 13:36:40 -0500

    Update Phone Numbers Logic
    Changed the ID for mobile phones for First Pres CO.  Added logic to get the phone type if not a standard phone type already in rock.

--- Slingshot.V5/SQL Queries/person-phone.sql
+++ Slingshot.V5/SQL Queries/person-phone.sql
@@ -6,14 +6,14 @@ SELECT
 		WHEN '1' THEN 'Home'
 		WHEN '2' THEN 'Business'
 		WHEN '7' THEN 'Business'
-		WHEN '80' THEN 'Mobile'
-		ELSE 'Home'
+		WHEN '276' THEN 'Mobile'
+		ELSE t.[Descr]
 		END AS [PhoneType]
 	-- PhoneNumber --
 	,ISNULL(NULLIF(REPLACE(REPLACE(ph.PhoneNu, CHAR(13), ''), CHAR(10), ''), '0'), '') AS [PhoneNumber]
 	-- IsMessageingEnabled --
 	,CASE ph.PhoneCounter
-		When 80 THEN 'TRUE'
+		When 276 THEN 'TRUE'
 		ELSE 'FALSE'
 	END AS [IsMessagingEnabled]
 	-- IsUnlisted --
@@ -21,9 +21,11 @@ SELECT
 		WHEN '0' THEN 'FALSE'
 		ELSE 'TRUE' -- IN V5, -1 and -2 both mean Unlisted
 	END AS [IsUnlisted]
+	, ph.PhoneCounter
 
 FROM Shelby.NANames n
 INNER JOIN Shelby.NAPhones AS ph ON n.NameCounter = ph.NameCounter
+INNER JOIN [Shelby].[NAPhoneTypes] as t on t.[Counter] = ph.PhoneCounter
 
 WHERE N.NameCounter IS NOT NULL
 ORDER BY PersonId DESC
