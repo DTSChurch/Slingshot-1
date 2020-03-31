fde501f212d7cefa2aef3799990082b15ffa0011
Info <Info@DTSChurch.com>
2020-03-31 15:22:33 -0500

    Updated Attendance to only grab attendance for date range and current groups.

--- Slingshot.V5/SQL Queries/attendance.sql
+++ Slingshot.V5/SQL Queries/attendance.sql
@@ -1,3 +1,6 @@
+-- This is the date we use to grab only recent groups
+DECLARE @StartDate DateTime = '2019-01-01';
+
 select
 AttendanceDetails.Counter as [AttendanceId]
 ,[NameCounter] as PersonId
@@ -8,14 +11,22 @@ AttendanceDetails.Counter as [AttendanceId]
 , OrgStats.SGDate AS [StartDateTime]
 ,'' AS [EndDateTime]
 ,'' AS [Note]
-,1 AS [CampusId]
+,'' AS [CampusId]
 from
  Shelby.SGMstOrgAtt AttendanceDetails inner join
  Shelby.SGMstOrg Enrollments on AttendanceDetails.SGMstOrgCounter = Enrollments.Counter inner join
  Shelby.SGOrgGrpDat GroupStats on AttendanceDetails.SGOrgGrpDatCounter = GroupStats.Counter inner join
  Shelby.SGOrgDat OrgStats ON GroupStats.SGOrgDatCounter = OrgStats.Counter left join
  Shelby.SGOrg as Organizations on OrgStats.SGOrgCounter = Organizations.Counter
- where 
- Organizations.Descr like '%' and SGAttTyp = 'P'
+ where SGAttTyp = 'P' -- Grab only people marked Present --
+	-- Grab only active groups in the date rage --
+	AND GroupStats.SGOrgGrpCounter IN (
+				SELECT	grp.[Counter] AS [Id]
+					FROM [Shelby].[SGOrgGrp] grp
+					LEFT OUTER JOIN [Shelby].[SGOrgLvl] lvl ON grp.[SGOrgLvlCounter] = lvl.[Counter]
+					LEFT OUTER JOIN [Shelby].[SGOrg] org ON lvl.[SGOrgCounter] = org.[Counter]
+					WHERE grp.WhenUpdated >= @StartDate
+					Group By grp.[Counter],grp.[Descr],grp.[SGOrgLvlCounter],lvl.SGOrgCounter,org.Descr
+				)
  order by AttendanceId
  
